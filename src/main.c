85e5cb56d6050142f20e7cff301185ec51e7955e
David Vandensteen <dvandensteen@gmail.com>
2023-07-02 14:01:37 +0200

    Add : ia screen limit

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <neocore.h>
 #include "externs.h"
 
+#define SCREEN_Y_MIN 0
+#define SCREEN_Y_MAX 256
+
 #define RACQUET_SPEED 3
+#define IA_STATE_TIMEOUT 10
 
-GFX_Picture_Physic racquet[2];
-paletteInfo racquet2_asset_Palettes;
+static enum state { IDLE, UP, DOWN };
 
+static GFX_Picture_Physic racquet[2];
+static paletteInfo racquet2_asset_Palettes;
+
+static enum state ia_state = IDLE;
+static int ia_state_timeout = IA_STATE_TIMEOUT;
 
 
 // ----------------------------------------patch neocore
@@ -45,13 +53,13 @@ static void update_player() {
 }
 
 static void update_ia() {
-  if (get_frame_counter() % 5 == 0) {
-    if (get_random(2) == 0) {
-      move_gpp(&racquet[1], 0, RACQUET_SPEED);
-    } else {
-      move_gpp(&racquet[1], 0, -RACQUET_SPEED);
-    }
+  if (ia_state_timeout <= 0) {
+    ia_state = get_random(3);
+    ia_state_timeout = IA_STATE_TIMEOUT;
   }
+  if (ia_state == UP && get_y_gpp(racquet[1]) > SCREEN_Y_MIN) move_gpp(&racquet[1], 0, -RACQUET_SPEED);
+  if (ia_state == DOWN && get_y_gpp(racquet[1]) < SCREEN_Y_MAX) move_gpp(&racquet[1], 0, RACQUET_SPEED);
+  ia_state_timeout -= 1;
 }
 
 static void update() {
