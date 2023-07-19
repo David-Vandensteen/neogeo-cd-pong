a51cb7c12277dbb079561c42edb88b9a8d3b5d8c
David Vandensteen <dvandensteen@gmail.com>
2023-07-19 16:31:43 +0200

    Add : wall collider

--- src/main.c
+++ src/main.c
@@ -10,30 +10,23 @@
 #define SCREEN_X_MIN 0
 #define SCREEN_X_MAX 320
 #define SCREEN_Y_MIN 0
-#define SCREEN_Y_MAX 256
+#define SCREEN_Y_MAX 224
 
 #define RACQUET_SPEED 4
 #define BALL_SPEED 2
 #define IA_DIRECTION_TIMEOUT 10
 
-// patch neocore
-typedef struct QuadShort {
-  Vec2short vector0;
-  Vec2short vector1;
-} QuadShort;
-//
-
 // todo : neocore expose direction ?
 enum direction { IDLE, UP, DOWN, LEFT, RIGHT };
 
 typedef struct BallState {
-  QuadShort border_limit;
-  Vec2short destination;
   Vec2short position;
   short slope;
   enum direction direction;
 } BallState;
 
+static Box upper_wall;
+static Box lower_wall;
 
 static GFX_Picture_Physic racquet[2];
 static GFX_Picture_Physic ball;
@@ -47,16 +40,6 @@ BOOL each_frame(DWORD frame) {
   return (get_frame_counter() % frame == 0) ? true : false;
 }
 
-// todo : deprecated neocore box ?
-QuadShort quad_short(short p0, short p1, short p2, short p3) {
-  Vec2short v0 = {p0, p1};
-  Vec2short v1 = {p2, p3};
-  QuadShort quad;
-  quad.vector0 = v0;
-  quad.vector1 = v1;
-  return quad;
-}
-
 Vec2short get_vec2_pos_gpp (GFX_Picture_Physic gfx_picture_physic) {
   Vec2short pos = {get_x_gpp(gfx_picture_physic), get_y_gpp(gfx_picture_physic)};
   return pos;
@@ -66,15 +49,8 @@ void set_vec2_pos_gpp(GFX_Picture_Physic *gfx_picture_physic, Vec2short pos) {
   set_pos_gpp(gfx_picture_physic, pos.x, pos.y);
 }
 
-Vec2short get_next_pos_slope(Vec2short current_pos, Vec2short destination_limit_pos, short slope, enum direction direction) {
+Vec2short get_next_pos_slope(Vec2short current_pos, short slope, enum direction direction) {
   Vec2short next_pos = {current_pos.x, current_pos.y};
-
-  if (direction == RIGHT) {
-    if (current_pos.x >= destination_limit_pos.x || current_pos.y >= destination_limit_pos.y) return current_pos;
-  } else if (direction == LEFT) {
-    if (current_pos.x <= destination_limit_pos.x || current_pos.y <= destination_limit_pos.y) return current_pos;
-  }
-
   if (slope != 0 && current_pos.x % slope == 0) next_pos.y = (slope > 0) ? next_pos.y + 1 : next_pos.y - 1;
   next_pos.x = (direction == RIGHT) ? next_pos.x + 1 : next_pos.x - 1;
   return next_pos;
@@ -85,14 +61,13 @@ static void init_racquet(GFX_Picture_Physic *racquet) {
   init_gpp(racquet, &racquet_asset, &racquet_asset_Palettes, 16, 64, 0, 0, AUTOBOX);
 
   // --------------------------------------------------------neocore patch
-  racquet->gfx_picture.pixel_height = racquet->gfx_picture.pixel_height - 28;
+  racquet->gfx_picture.pixel_height = racquet->gfx_picture.pixel_height - 64;
   // --------------width patch ?
 }
 
 static void init_ball_state(BallState *ball_state) {
-  ball_state->slope = 0;
+  ball_state->slope = -5;
   ball_state->direction = RIGHT;
-  ball_state->border_limit = quad_short(SCREEN_X_MIN, SCREEN_Y_MIN, SCREEN_X_MAX - 32, SCREEN_Y_MAX);
 }
 
 static void init() {
@@ -101,27 +76,43 @@ static void init() {
   init_racquet(&racquet[1]);
   init_gpp(&ball, &ball_asset, &ball_asset_Palettes, 16, 16, 0, 0, AUTOBOX); // TODO : neocore : AUTOBOX not working
   init_ball_state(&ball_state);
+
+  init_box(&upper_wall, 320, 16, 0, 0);
+  update_box(&upper_wall, 0, -16);
+
+  init_box(&lower_wall, 320, 16, 0, 0);
+  update_box(&lower_wall, 0, 224);
 }
 
 static void display() {
   display_gpp(&racquet[0], 16, 16);
   display_gpp(&racquet[1], 320 - 32, 16);
-  display_gpp(&ball, 0, 10);
+  display_gpp(&ball, 10, 100);
 }
 
 static void update_ball() {
+  BOOL collide_player = collide_box(&ball.box, &racquet[0].box);
+  BOOL collide_ia = collide_box(&ball.box, &racquet[1].box);
+  BOOL collide_upper_wall = collide_box(&upper_wall, &ball.box);
+  BOOL collide_lower_wall = collide_box(&lower_wall, &ball.box);
+
   ball_state.position = get_vec2_pos_gpp(ball);
 
-  if (ball_state.direction == RIGHT) ball_state.destination = ball_state.border_limit.vector1;
-  if (ball_state.direction == LEFT) ball_state.destination = ball_state.border_limit.vector0;
-
   init_log();
+  log_short("X", ball_state.position.x);
+  log_short("Y", ball_state.position.y);
 
-  if (ball_state.position.x <= ball_state.border_limit.vector0.x) {
+  if (
+      collide_upper_wall && ball_state.slope < 0
+      || collide_lower_wall && ball_state.slope > 0
+    ) ball_state.slope = ball_state.slope * -1;
+
+  if (collide_player || ball_state.position.x <= SCREEN_X_MIN) {
     ball_state.direction = RIGHT;
+    ball_state.slope = (short)get_random(10);
   }
 
-  if (ball_state.position.x >= ball_state.border_limit.vector1.x) {
+  if (collide_ia || ball_state.position.x >= SCREEN_X_MAX) {
     ball_state.direction = LEFT;
   }
 
@@ -129,7 +120,6 @@ static void update_ball() {
     &ball,
     get_next_pos_slope(
       ball_state.position,
-      ball_state.destination,
       ball_state.slope,
       ball_state.direction
     )
